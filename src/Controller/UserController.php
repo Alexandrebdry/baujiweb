f5c82453ce4b0f977f888e6e8f9b21e6762f46c4
Guillaume <gui.jolly78@gmail.com>
2021-11-11 01:37:02 +0100

    User formulaire

---
+++ src/Controller/UserController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserAddFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user/", name="user")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Route("/user/add", name="user_add")
+     */
+    public function addUser(Request $request){
+        $user= new User();
+        $form= $this->createForm(UserAddFormType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+        }
+        return $this->render('user/add.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
+
---
+++ src/Form/UserAddFormType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserAddFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('birthdate')
+            ->add('password')
+            ->add('email')
+            ->add('role')
+            ->add('created_at')
+            ->add('updated_at')
+            ->add('isActivate')
+            ->add('structure')
+            ->add('commandes')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
